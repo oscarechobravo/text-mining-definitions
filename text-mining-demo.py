aace8865b7d05eedb415d364f951fe08d4932202
Oliver Bates <oliver.bates@gmail.com>
2017-08-24 15:06:37 +0100

    started coding..

--- text-mining-demo.py
+++ text-mining-demo.py
@@ -4,4 +4,11 @@
 
 # https://datawarrior.wordpress.com/2016/12/22/python-package-for-short-text-mining/
 
+# https://gist.github.com/fnielsen/3603279
+
 # Text mining for SHCI paper
+
+
+import nltk
+import csv
+import json
